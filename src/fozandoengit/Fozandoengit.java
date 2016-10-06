5d4192a2c022292d576d2ff5c848560c6173aaea
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-10-06 08:57:31 +0200

    fozar1

---
+++ src/fozandoengit/Fozandoengit.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fozandoengit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author otorradomiguez
+ */
+public class Fozandoengit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float base;
+        float altura;
+        base = Float.parseFloat(JOptionPane.showInputDialog("Introduce la base:"));
+        altura = Float.parseFloat(JOptionPane.showInputDialog("Introduce la altura"));
+        JOptionPane.showMessageDialog(null, "El area es: "+(base*altura)/2);
+    }
+    
+}
