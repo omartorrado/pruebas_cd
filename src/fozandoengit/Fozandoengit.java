2af774ae72de4273541ac988c49e4fc0b5dda40e
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-11-07 09:13:40 +0100

    medificamos inicialmente. prueba de conflicto

--- src/fozandoengit/Fozandoengit.java
+++ src/fozandoengit/Fozandoengit.java
@@ -18,14 +18,16 @@ public class Fozandoengit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        float base;
-        float altura;
-        base = Float.parseFloat(JOptionPane.showInputDialog("Introduce la base del triangulo:"));
-        altura = Float.parseFloat(JOptionPane.showInputDialog("Introduce la altura del triangulo:"));
-        JOptionPane.showMessageDialog(null, "El area del triangulo es: "+(base*altura)/2);
-        
+        float base=2;
+        float altura=5;
+//        base = Float.parseFloat(JOptionPane.showInputDialog("Introduce la base del triangulo:"));
+//        altura = Float.parseFloat(JOptionPane.showInputDialog("Introduce la altura del triangulo:"));
+//        JOptionPane.showMessageDialog(null, "El area del triangulo es: "+(base*altura)/2);
+//        
         //Comentario random sobre el programa. modificacion 1
         //necesidades basicas incompletas. rendimiento 20%
+        System.out.println(base*altura);
+        
     }
     
 }
